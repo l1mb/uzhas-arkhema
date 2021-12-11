effd130512c269a0b3e1ef57b514d787714f3b5b
Yasper Mag <sergeev.s1012@gmail.com>
2021-12-11 12:32:22 +0300

    add product page

--- black/src/components/products/products.tsx
+++ black/src/components/products/products.tsx
@@ -1,12 +1,25 @@
 import { useEffect, useState } from "react";
 import Categories from "./categories/categories";
+import FilterBar from "./FilterBar/filter";
+import ProductCard, { ProductCardProps } from "./productCard/productCard";
 import styles from "./style.module.scss";
 
 function Products() {
   const catData = ["all", "dlya bichey", "chisto multiki", "poigrat'"];
-
   const [categorie, setCategorie] = useState(catData[0].toUpperCase());
-  useEffect(() => {}, [categorie]);
+
+  const data: ProductCardProps[] = [
+    { label: "Acer aspire", shortDescription: "nu zaebis noutbuk, nu ohuenniy", price: "750$" },
+    { label: "Acer aspire", shortDescription: "nu zaebis noutbuk, nu ohuenniy", price: "750$" },
+    { label: "Acer aspire", shortDescription: "nu zaebis noutbuk, nu ohuenniy", price: "750$" },
+    { label: "Acer aspire", shortDescription: "nu zaebis noutbuk, nu ohuenniy", price: "750$" },
+    { label: "Acer aspire", shortDescription: "nu zaebis noutbuk, nu ohuenniy", price: "750$" },
+    { label: "Acer aspire", shortDescription: "nu zaebis noutbuk, nu ohuenniy", price: "750$" },
+  ];
+
+  useEffect(() => {
+    // TODO: fetch data
+  }, [categorie]);
   return (
     <div className={styles.extraDiv}>
       <div className={styles.page_wrapper}>
@@ -16,6 +29,17 @@ function Products() {
             {catData.map((elem) => (
               <Categories label={elem.toUpperCase()} setValue={(e: string) => setCategorie(e)} selected={categorie} />
             ))}
+          </div>
+
+          <div className={styles.contentRow}>
+            <div>
+              <FilterBar setQuery={(e: string) => {}} categorie={categorie} />
+            </div>
+            <div className={styles.cards}>
+              {data.map((elem) => (
+                <ProductCard label={elem.label} price={elem.price} shortDescription={elem.shortDescription} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
