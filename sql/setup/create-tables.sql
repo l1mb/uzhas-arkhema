9451f187c45cfb5c5ec77393e7968fb7ad74612f
-hasty- <tikhomirovtimofei@gmail.com>
2021-12-15 17:46:40 +0300

    Add order related tables

--- sql/setup/create-tables.sql
+++ sql/setup/create-tables.sql
@@ -1,3 +1,24 @@
+drop table orders_products;
+create table orders_products(
+    order_id number not null,
+    product_id number not null,
+    constraint orders_products_unq unique(order_id, product_id)
+);
+
+drop table orders;
+create table orders (
+    id number generated always as identity,
+    user_id number not null,
+    phone varchar2(15),
+    order_date date default sysdate,
+    rent_start_date date not null,
+    rent_end_date date not null,
+    cost number(10, 4) not null,
+    status varchar2(50) default 'notapproved',
+    constraint orders_pk primary key (id),
+    constraint orders_chk check (status in ('not approved', 'approved', 'rejected', 'completed'))
+);
+
 drop table users;
 create table users (
 	id number generated always as identity,
@@ -35,5 +56,8 @@ create table vendors (
 	constraint vendors_pk primary key (id)
 );
 
-alter table products add constraint products_category_fk foreign key (category_id) references categories(id);
-alter table products add constraint products_vendor_fk foreign key (vendor_id) references vendors(id);
+alter table products add constraint products_categories_fk foreign key (category_id) references categories(id);
+alter table products add constraint products_vendors_fk foreign key (vendor_id) references vendors(id);
+alter table orders add constraint orders_users_fk foreign key (user_id) references users(id);
+alter table orders_products add constraint orders_products_orders_fk foreign key (order_id) references orders(id);
+alter table orders_products add constraint orders_products_products_fk foreign key (product_id) references products(id);
