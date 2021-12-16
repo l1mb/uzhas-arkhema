a67c3dce2fa591358e353d6416fd0f42e24f511d
black <sergeev.mark@outlook.com>
2021-12-16 22:23:19 +0300

    restyled buttons

--- black/src/components/footerComponent/footer.tsx
+++ black/src/components/footerComponent/footer.tsx
@@ -1,41 +1,43 @@
 import styles from "./footer.module.scss";
-import eaIcon from "../../assets/images/footerIcons/ea.png";
-import valveIcon from "../../assets/images/footerIcons/valve.png";
-import riotIcon from "../../assets/images/footerIcons/riot.png";
-import rockIcon from "../../assets/images/footerIcons/rockstar.png";
-import ubicIcon from "../../assets/images/footerIcons/ubic.png";
+import fender from "../../assets/images/footerIcons/fender.svg";
+import gibson from "../../assets/images/footerIcons/gibson.svg";
+import squire from "../../assets/images/footerIcons/squire.svg";
+import jackson from "../../assets/images/footerIcons/jackson.svg";
+import ibanez from "../../assets/images/footerIcons/ibanez.svg";
 
-const Footer = (): JSX.Element => (
-  <footer className={styles.footer}>
-    <span>Incredible convenient</span>
-    <div className={styles.icons}>
-      <div className={styles.footerLogo}>
-        <a href="https://www.ea.com/">
-          <img src={eaIcon} alt="EA" />
-        </a>
+function Footer(): JSX.Element {
+  return (
+    <footer className={styles.footer}>
+      <span>Incredible convenient</span>
+      <div className={styles.icons}>
+        <div className={styles.footerLogo}>
+          <a href="https://www.fender.com/">
+            <img src={fender} alt="fender" />
+          </a>
+        </div>
+        <div className={styles.footerLogo}>
+          <a href="https://www.gibson.com">
+            <img src={gibson} alt="Valve" />
+          </a>
+        </div>
+        <div className={styles.footerLogo}>
+          <a href="https://www.fender.com/en/squier-electric-guitars/">
+            <img src={squire} alt="RiotProducts" />
+          </a>
+        </div>
+        <div className={styles.footerLogo}>
+          <a href="https://www.jacksonguitars.com/en/start">
+            <img src={jackson} alt="RockStar" />
+          </a>
+        </div>
+        <div className={styles.footerLogo}>
+          <a href="https://www.ibanez.com/">
+            <img src={ibanez} alt="Ubisoft" />
+          </a>
+        </div>
       </div>
-      <div className={styles.footerLogo}>
-        <a href="https://www.valvesoftware.com/en/">
-          <img src={valveIcon} alt="Valve" />
-        </a>
-      </div>
-      <div className={styles.footerLogo}>
-        <a href="https://www.riotProducts.com/en">
-          <img src={riotIcon} alt="RiotProducts" />
-        </a>
-      </div>
-      <div className={styles.footerLogo}>
-        <a href="https://www.rockstarProducts.com/">
-          <img src={rockIcon} alt="RockStar" />
-        </a>
-      </div>
-      <div className={styles.footerLogo}>
-        <a href="https://www.ubisoft.com/">
-          <img src={ubicIcon} alt="Ubisoft" />
-        </a>
-      </div>
-    </div>
-  </footer>
-);
+    </footer>
+  );
+}
 
 export default Footer;
