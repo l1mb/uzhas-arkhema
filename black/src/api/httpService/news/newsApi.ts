833bb8106be9ba7373fbba973f23c9b16ba73369
black mask <sergeev.s1012@gmail.com>
2021-12-18 09:40:52 +0000

    add news api

--- black/src/api/httpService/news/newsApi.ts
+++ black/src/api/httpService/news/newsApi.ts
@@ -1,9 +1,8 @@
 import endpoints from "@/api/endpoints";
-import IBasicProduct from "@/api/types/products/IBasicProduct";
-import postOrderEntity from "@/types/interfaces/order/postOrderEntity";
-import OrderProduct from "@/types/interfaces/order/orderProducts";
 import getOptions from "../tokenedOptions";
 import mnfrReadDto from "@/types/interfaces/news/nmfrs";
+import updateNews from "@/types/interfaces/news/updateNews";
+import postNews from "@/types/interfaces/news/postNews";
 
 
 const getMnfrs = async (): Promise<mnfrReadDto[]> => {
@@ -12,19 +11,19 @@ const getMnfrs = async (): Promise<mnfrReadDto[]> => {
   return data.json();
 };
 
-const postNews = async (prop: postOrderEntity): Promise<Response> => {
+const postNews = async (prop: postNews): Promise<Response> => {
   const data = await fetch(`${endpoints.news}`, getOptions("POST", true, prop));
 
   return data;
 };
 
-const deleteNews = async (prop: { keys: number[] }): Promise<Response> => {
+const deleteNews = async (prop: { keys: number }): Promise<Response> => {
   const data = await fetch(`${endpoints.news}`, getOptions("DELETE", true, prop));
 
   return data;
 };
 
-const putNews = async (body: BackendOrderUpdateEntity): Promise<Response> => {
+const putNews = async (body: updateNews): Promise<Response> => {
   const data = await fetch(`${endpoints.news}`, getOptions("PUT", true, body));
   return data;
 };
