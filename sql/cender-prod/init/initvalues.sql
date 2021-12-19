589bed0bc554f8286653edeb20dd7028447323d3
black mask <sergeev.s1012@gmail.com>
2021-12-19 13:50:59 +0000

    add default inserts

---
+++ sql/cender-prod/init/initvalues.sql
@@ -0,0 +1,14 @@
+
+insert into pickups(name) values ('H-H');
+insert into pickups(name) values ('S-H');
+insert into pickups(name) values ('S-S-H');
+insert into pickups(name) values ('S-S-S');
+insert into pickups(name) values ('Y-A-H');
+
+
+
+insert into manufacturers(name) values ('fender');
+insert into manufacturers(name) values ('ibanez');
+insert into manufacturers(name) values ('jackson');
+insert into manufacturers(name) values ('gibson');
+insert into manufacturers(name) values ('squire');
