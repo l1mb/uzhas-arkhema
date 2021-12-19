325aaa66e2800d2572ff843701c754506292ea2e
black mask <sergeev.s1012@gmail.com>
2021-12-19 09:39:14 +0000

    start changing procedures

--- sql/cender-prod/news.sql
+++ sql/cender-prod/news.sql
@@ -1,8 +1,8 @@
 create or replace package cender_news as
     procedure get_news(out_news out sys_refcursor);
     procedure create_news(
-        in_news in news.news%type,
-        in_created_at in news.created_at%type);
+        in_manufacturer_id in manufacturers.id%type, 
+        in_news in news.news%type);
     procedure update_news(
         in_news_id in news.id%type,
         in_news_news in news.news%type);
@@ -21,12 +21,14 @@ create or replace package body cender_news
     end;
 
     procedure create_news(
-        in_news in news.news%type,
-        in_created_at in news.created_at%type
-        )
+        in_manufacturer_id in manufacturers.id%type, 
+        in_news in news.news%type)
         as
+            news_id news.id%type;
         begin
-            insert into news(news, created_at) values (in_news, in_created_at);
+            insert into news(news) values (in_news);
+            select id into news_id from news where news.news = in_news;
+            insert into manufacturers(newsId) values (news_id);
         exception
             when others then
                 rollback;
