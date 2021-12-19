aefcfb590601c889de9ea920fd55cfda6c53ec12
black mask <sergeev.s1012@gmail.com>
2021-12-19 07:59:43 +0000

    add methods to manufacturers controller

--- src/controllers/manufacturerController.js
+++ src/controllers/manufacturerController.js
@@ -19,7 +19,41 @@ const getById = async (_, res, next) => {
     }
 }
 
+const postNews = async (req, res, next) => {
+    try {
+        const { manufacturerId, news } = req.body
+        const manufacturer = await manufacturerRepository.createNews(manufacturerId, news)
+        return res.status(200).json(manufacturer)
+    } catch (err) {
+        next(err)
+    }
+}
+
+const putNews = async (req, res, next) => {
+    try {
+        const { id, news } = req.body
+        const manufacturer = await manufacturerRepository.updateNews(id, news)
+        return res.status(200).json(manufacturer)
+    } catch (err) {
+        next(err)
+    }
+}
+
+const deleteNews = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const manufacturer = await manufacturerRepository.deleteNewsById(id)
+        return res.status(200).json(manufacturer)
+    } catch (err) {
+        next(err)
+    }
+}
+
+
 module.exports = {
     getAll,
     getById,
+    postNews,
+    putNews,
+    deleteNews
 }
